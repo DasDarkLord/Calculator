594c2108bccb590944a6e94bb48d952ee6926c26
DasDarkLord <82238969+DasDarkLord@users.noreply.github.com>
2023-10-19 02:19:06 +0200

    arrow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,10 +11,13 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     while (true) {
+        print("> ")
         val user = scanner.nextLine()
 
         // Lex Input
         val tokens = Lexer(user).lexTokens()
+
+        print(">> ")
         printTokens(tokens) // print colored version of input
 
         // Parse to a Tree
