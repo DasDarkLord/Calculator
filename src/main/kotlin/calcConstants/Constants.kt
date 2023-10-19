d466338a5e5e9babfc16da380ec9ec734f440460
DasDarkLord <82238969+DasDarkLord@users.noreply.github.com>
2023-10-19 02:49:09 +0200

    infinity and nan xdd

--- src/main/kotlin/calcConstants/Constants.kt
+++ src/main/kotlin/calcConstants/Constants.kt
@@ -3,6 +3,8 @@ package calcConstants
 val constants = mapOf<List<String>, Any>(
     listOf("pi") to 3.14159,
     listOf("e", "euler") to 2.71828,
+    listOf("infinity", "inf", "positive_infinity") to Double.POSITIVE_INFINITY,
+    listOf("NaN", "nan", "NAN") to Double.NaN
 )
 
 val userConstants = HashMap<List<String>, Any>()
